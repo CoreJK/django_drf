41061ccae8a2666c2debad0a16b726c922c4dc5b
coreJK <r465755956@gmail.com>
2025-03-25 16:45:19 +0800

    refactor: 使用 mixin 重写通过的增删查改方法一

--- drf_vue_blog/article/views.py
+++ drf_vue_blog/article/views.py
@@ -1,41 +1,27 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
-
-from django.http import Http404
+from rest_framework import mixins
+from rest_framework import generics
 
 from article.models import Article
 from article.serializers import ArticleDetailSerializer
 
 # Create your views here.
 
-class ArticleDetail(APIView):
+class ArticleDetail(mixins.RetrieveModelMixin, 
+                    mixins.UpdateModelMixin, 
+                    mixins.DestroyModelMixin, 
+                    generics.GenericAPIView):
     """文章详情视图"""
+    queryset = Article.objects.all()
+    serializer_class = ArticleDetailSerializer
     
-    def get_object(self, pk):
-        """获取单个文章对象"""
-        try:
-            return Article.objects.get(pk=pk)
-        except:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
     
-    def get(self, request, pk):
-        """获取接口"""
-        article = self.get_object(pk)
-        serializer = ArticleDetailSerializer(article)
-        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
     
-    def put(self, request, pk):
-        """更新文章接口"""
-        article = self.get_object(pk)
-        serializer = ArticleDetailSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
     
-    def delete(self, request, pk):
-        """删除文章接口"""
-        article = self.get_object(pk)
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    
